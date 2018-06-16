69d4f87a787c36aca904c23e8e2ffe5339b067d4
Kalana <kalana@sp-solutions.biz>
2018-06-16 10:10:06 +0530

    initialcommit

---
+++ src/main/java/com/kalana/activiti7/Activiti7Application.java
@@ -0,0 +1,12 @@
+package com.kalana.activiti7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activiti7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Activiti7Application.class, args);
+    }
+}
